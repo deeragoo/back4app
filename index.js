5aaa754273019636043869e973d1b231eb8adb4c
ragoosterman <dee.ragoo@icloud.com>
2022-12-19 18:46:07 -0500

    added parse package

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
+require('dotenv').config()
 var Parse = require('parse/node');
 
-Parse.initialize("APP_ID","JS_KEY"); //PASTE HERE YOUR Back4App APPLICATION ID AND YOUR JavaScript KEY
+Parse.initialize(process.env.APP_ID, process.env.JS_KEY); //PASTE HERE YOUR Back4App APPLICATION ID AND YOUR JavaScript KEY
 Parse.serverURL = 'https://parseapi.back4app.com/'
 
 
 async function saveNewPlayer() {
   //Create your Parse Object
-  const soccerPlayer = new Parse.Object('SoccerPlayer');
+  const user = new Parse.Object('User');
   //Define its attributes
-  soccerPlayer.set('playerName', 'A. Wed');
-  soccerPlayer.set('yearOfBirth', 1997);
-  soccerPlayer.set('emailContact', 'a.wed@email.io');
-  soccerPlayer.set('attributes', ['fast', 'good conditioning']);
+  user.set('playerName', 'A. Wed');
+  user.set('yearOfBirth', 1997);
+  user.set('emailContact', 'a.wed@email.io');
+  user.set('attributes', ['fast', 'good conditioning']);
   try {
     //Save the Object
-    const result = await soccerPlayer.save();
+    const result = await user.save();
     alert('New object created with objectId: ' + result.id);
   } catch (error) {
     alert('Failed to create new object: ' + error.message);
